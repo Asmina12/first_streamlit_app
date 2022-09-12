44a1a9abb597cc63439189e71d580e552998b2c5
AsmiHUb <113334590+Asmina12@users.noreply.github.com>
2022-09-12 18:13:35 +0530

    Update streamlit_app.py
    Import Pandas Library to read an csv file from S3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,9 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+
+My_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe (My_fruit_list)
